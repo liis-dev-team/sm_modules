bc95e70a1687dcd1c6ae1d93d60bda943a8534e3
dolphinLesha <alyksey11@gmail.com>
2024-02-20 16:51:20 +0300

    add sm_vector library

---
+++ lib/sm_vector.dart
@@ -0,0 +1,5 @@
+library sm_vector;
+
+export 'package:sm_modules/src/ui_kit/helpers/vector/vector2/vector2.dart';
+export 'package:sm_modules/src/ui_kit/helpers/vector/vector3/vector3.dart';
+export 'package:sm_modules/src/ui_kit/helpers/vector/converter.dart';
