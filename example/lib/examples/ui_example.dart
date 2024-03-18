2c26f89ff5870a0d2f002e12a430823a12c53fb9
dolphinLesha <alyksey11@gmail.com>
2024-03-18 11:07:13 +0300

    update dropdown input

--- example/lib/examples/ui_example.dart
+++ example/lib/examples/ui_example.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portal/flutter_portal.dart';
 import 'package:sm_modules/sm_ui_kit.dart';
 
 class UIExamples extends StatelessWidget {
@@ -9,14 +10,45 @@ class UIExamples extends StatelessWidget {
     return MaterialApp(
       title: 'AuthFormExample',
       theme: AppThemes.lightThemeMobile(),
-      home: SingleChildScrollView(
-        child: Column(
-          children: [
-            DropdownButtonFormField(
-              items: [],
-              onChanged: (_) {},
-            ),
-          ],
+      home: Portal(
+        child: Builder(
+          builder: (context) {
+            ThemeData theme = Theme.of(context);
+            SMColors colors = theme.extension<SMColors>()!;
+            return Scaffold(
+              body: SingleChildScrollView(
+                child: Center(
+                  child: SizedBox(
+                    width: 300,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        DropdownInput<String>(
+                          label: 'label',
+                          menuAnchor: Alignment.bottomRight,
+                          childAnchor: Alignment.topRight,
+                          items: [
+                            'first',
+                            'second',
+                          ],
+                        ),
+                        DropdownInput<String>(
+                          label: 'label',
+                          menuAnchor: Alignment.bottomRight,
+                          childAnchor: Alignment.topRight,
+                          items: [
+                            'first',
+                            'second',
+                          ],
+                          textColor: colors.markerYellow,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
         ),
       ),
     );
